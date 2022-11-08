931089620bfff7ac3364e31994ca7e0f2d2e3c44
beatrizDuarte <beatriznduarte1@gmail.com>
2022-11-07 21:05:03 -0300

    Feature: Create Ad Modal

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,4 +1,5 @@
-import { Image, TouchableOpacity, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, Image, TouchableOpacity, View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useRoute, useNavigation } from '@react-navigation/native';
 import { Entypo } from '@expo/vector-icons';
@@ -12,7 +13,7 @@ import { THEME } from '../../theme';
 
 import { GameParams } from '../../@types/navigation';
 import { Heading } from '../../components/Heading';
-import { DuoCard } from '../../components/DuoCard';
+import { DuoCard, DuoCardProps } from '../../components/DuoCard';
 
 interface RouteParams {
   id: string;
@@ -22,6 +23,8 @@ interface RouteParams {
 
 export function Game() {
 
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
+
   const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
@@ -29,6 +32,14 @@ export function Game() {
   function handleGoBack(){
     navigation.goBack();
   }
+
+  useEffect(() => {
+    fetch(`http://192.168.0.13:3333/games/${game.id}/ads`)
+    .then(response => response.json())
+    .then(data => {
+      setDuos(data);
+    });
+  }, []);
 
   return (
     <Background>
@@ -61,7 +72,25 @@ export function Game() {
         subtitle="Conecte-se e comece a jogar"
       />
 
-      <DuoCard />
+      <FlatList 
+        data={duos}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <DuoCard 
+            data={item}
+            onConnect={() => {}}
+          />
+        )}
+        horizontal
+        style={styles.containerList}
+        contentContainerStyle={[duos.length > 0 ? styles.contentList : styles.emptyListContent]}
+        showsHorizontalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.emptyListText}>
+            Não há anúncios publicados ainda.
+          </Text>
+        )}
+      />
       </SafeAreaView>
     </Background>
   );
